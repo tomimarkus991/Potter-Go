fe4de1af263d39a8a4e784c811469bda59c9ace3
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-08-05 11:29:00 +0300

    finish navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -17,12 +17,15 @@ import {
   SortingHatHover,
 } from "../../assets/icons/navbarHover/NavbarHover";
 
+import { motion } from "framer-motion";
+
 const Navbar = () => {
   const [homeTrainIcon, setHomeTrainIcon] = useState(HomeRegular);
   const [wizardIcon, setWizardIcon] = useState(WizardsRegular);
   const [potionsIcon, setPotionsIcon] = useState(PotionsRegular);
   const [housesIcon, setHousesIcon] = useState(HousesRegular);
   const [sortingHatIcon, setSortingHatIcon] = useState(SortingHatRegular);
+
   return (
     <nav className="navbar">
       <span
@@ -34,16 +37,25 @@ const Navbar = () => {
       >
         <i className="fas fa-bars"></i>
       </span>
-      <li className="nav-logo">
+      <motion.li className="nav-logo">
         <Link className="nav-link" to="/">
-          <img className="nav-img" src={HogwartsRegular} alt="Hogwarts" />
+          <motion.img
+            className="nav-img"
+            src={HogwartsRegular}
+            alt="Hogwarts"
+          />
           <span className="nav-text">Harry Potter</span>
         </Link>
-      </li>
+      </motion.li>
       <ul className="navbar-nav">
-        <li
-          onMouseEnter={() => setHomeTrainIcon(HomeHover)}
-          onMouseLeave={() => setHomeTrainIcon(HomeRegular)}
+        <motion.li
+          whileHover={{
+            backgroundColor: "#3a3b3c",
+            scale: 1.2,
+          }}
+          transition={{ duration: 0.1 }}
+          onHoverStart={() => setHomeTrainIcon(HomeHover)}
+          onHoverEnd={() => setHomeTrainIcon(HomeRegular)}
         >
           <NavLink className="nav-link active" to="/">
             <img
@@ -52,39 +64,62 @@ const Navbar = () => {
               alt="Hogwarts Train"
             />
           </NavLink>
-        </li>
-        <li
-          onMouseEnter={() => setWizardIcon(WizardsHover)}
-          onMouseLeave={() => setWizardIcon(WizardsRegular)}
+        </motion.li>
+
+        <motion.li
+          whileHover={{
+            backgroundColor: "#3a3b3c",
+            scale: 1.2,
+          }}
+          transition={{ duration: 0.1 }}
+          onHoverStart={() => setWizardIcon(WizardsHover)}
+          onHoverEnd={() => setWizardIcon(WizardsRegular)}
         >
           <NavLink className="nav-link active" to="/wizards">
             <img className="nav-icon" src={wizardIcon} alt="Wizards" />
           </NavLink>
-        </li>
-        <li
-          onMouseEnter={() => setPotionsIcon(PotionsHover)}
-          onMouseLeave={() => setPotionsIcon(PotionsRegular)}
+        </motion.li>
+        <motion.li
+          whileHover={{
+            backgroundColor: "#3a3b3c",
+            scale: 1.2,
+          }}
+          transition={{ duration: 0.1 }}
+          onHoverStart={() => setPotionsIcon(PotionsHover)}
+          onHoverEnd={() => setPotionsIcon(PotionsRegular)}
         >
           <NavLink className="nav-link" to="/potions">
             <img className="nav-icon" src={potionsIcon} alt="Potions" />
           </NavLink>
-        </li>
-        <li
-          onMouseEnter={() => setHousesIcon(HousesHover)}
-          onMouseLeave={() => setHousesIcon(HousesRegular)}
+        </motion.li>
+
+        <motion.li
+          whileHover={{
+            backgroundColor: "#3a3b3c",
+            scale: 1.2,
+          }}
+          transition={{ duration: 0.1 }}
+          onHoverStart={() => setHousesIcon(HousesHover)}
+          onHoverEnd={() => setHousesIcon(HousesRegular)}
         >
           <NavLink className="nav-link" to="/houses">
             <img className="nav-icon" src={housesIcon} alt="Houses" />
           </NavLink>
-        </li>
-        <li
-          onMouseEnter={() => setSortingHatIcon(SortingHatHover)}
-          onMouseLeave={() => setSortingHatIcon(SortingHatRegular)}
+        </motion.li>
+
+        <motion.li
+          whileHover={{
+            backgroundColor: "#3a3b3c",
+            scale: 1.2,
+          }}
+          transition={{ duration: 0.1 }}
+          onHoverStart={() => setSortingHatIcon(SortingHatHover)}
+          onHoverEnd={() => setSortingHatIcon(SortingHatRegular)}
         >
           <NavLink className="nav-link" to="/sortingHat">
             <img className="nav-icon" src={sortingHatIcon} alt="Sorting Hat" />
           </NavLink>
-        </li>
+        </motion.li>
       </ul>
     </nav>
   );
