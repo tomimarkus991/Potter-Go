f1cb42c2c460aa1fe5e44ff3ec78e1edd06b12ba
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-08-18 19:41:42 +0300

    make picture loading faster

--- src/components/houses/HousesMain.js
+++ src/components/houses/HousesMain.js
@@ -1,17 +1,12 @@
 import React from "react";
 import Houses from "./Houses";
-import { motion } from "framer";
 
 const HousesMain = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.3 }}
-    >
+    <div>
       <h1 className="headline heading">Houses</h1>
       <Houses />
-    </motion.div>
+    </div>
   );
 };
 
